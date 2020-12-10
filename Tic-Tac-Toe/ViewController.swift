7d566555a5e4b2e1a9559e1ffd2d08e2a3bff97a
David Fisher <fisherds@rose-hulman.edu>
2020-12-10 13:16:01 -0500

    toolbar on iPhone or Nav bar on iPad

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
+  @IBAction func pressedNewGame(_ sender: Any) {
+    print("New Game")
+  }
 
 }
 
