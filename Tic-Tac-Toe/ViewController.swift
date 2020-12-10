f0bac915824d20e5b3c3fb858cea85188abf4077
David Fisher <fisherds@rose-hulman.edu>
2020-12-10 14:06:21 -0500

    finished TTT app

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -14,30 +14,60 @@ class ViewController: UIViewController {
   @IBOutlet weak var gameStateNavBar: UINavigationBar!
 
   @IBOutlet var gameBoardButtons: [UIButton]!
+  @IBOutlet weak var gameBoardImage: UIImageView!
+
+  var game = TicTacToeGame()
+  var xImage: UIImage!
+  var oImage: UIImage!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    if traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact {
+      // iPhone
+      gameBoardImage.image = #imageLiteral(resourceName: "iPhone_board.png")
+      xImage = #imageLiteral(resourceName: "iPhone_X.png")
+      oImage = #imageLiteral(resourceName: "iPhone_O.png")
+    } else {
+      // iPad
+      gameBoardImage.image = #imageLiteral(resourceName: "iPad_board.png")
+      xImage = #imageLiteral(resourceName: "iPad_X.png")
+      oImage = #imageLiteral(resourceName: "iPad_O.png")
+    }
     updateView()
   }
 
   @IBAction func pressedNewGame(_ sender: Any) {
     print("New Game")
-
-
+    game = TicTacToeGame()
     updateView()
   }
 
   @IBAction func pressedGameBoardButton(_ sender: Any) {
     let button = sender as! UIButton
     print(button.tag)
-
-
+    game.pressedSquareAt(button.tag)
     updateView()
   }
 
   func updateView() {
-
+    if traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact {
+      // iPhone
+      gameStateLabel.text = game.state.rawValue
+    } else {
+      // iPad
+      gameStateNavBar.topItem?.title = game.state.rawValue
+    }
+    for button in gameBoardButtons {
+      let buttonIndex = button.tag
+      switch game.board[buttonIndex] {
+      case .none:
+        button.setImage(nil, for: .normal)
+      case .x:
+        button.setImage(xImage, for: .normal)
+      case .o:
+        button.setImage(oImage, for: .normal)
+      }
+    }
   }
 }
 
